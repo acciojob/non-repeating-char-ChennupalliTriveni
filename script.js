4617c00239f09319ade35a5e55a6606d102b8cd3
Chennupalli Triveni <chennupallitriveni2003@gmail.com>
2025-11-16 11:34:02 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,22 @@
 function firstNonRepeatedChar(str) {
- // Write your code here
+  const freq = {};
+
+  // Count frequency of each character
+  for (let char of str) {
+    freq[char] = (freq[char] || 0) + 1;
+  }
+
+  // Find first character with frequency 1
+  for (let char of str) {
+    if (freq[char] === 1) {
+		
+      return char;
+    }
+  }
+
+  return null; // no non-repeated character
 }
+
+
 const input = prompt("Enter a string");
 alert(firstNonRepeatedChar(input)); 
